05e68db42c7c68962ebf7e85a19e2caa1412ec17
Skillado <drumond.guilherme@hotmail.com>
2020-10-15 07:41:20 -0300

    Aula 2 - Back end
    Feito em node.js, slite typescript e cors.

---
+++ back-end/src/controllers/OrphanagesController.ts
@@ -0,0 +1,86 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
+import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
+
+
+export default {
+  async index(request:Request,response:Response){
+    const orphanagesRespository = getRepository(Orphanage);
+
+    const orphanages= await orphanagesRespository.find({
+      relations:['images']
+    });
+
+    return response.json(orphanageView.renderMany(orphanages));
+  },
+  async show(request:Request,response:Response){
+    const { id } = request.params;
+
+    const orphanagesRespository = getRepository(Orphanage);
+
+    const orphanage= await orphanagesRespository.findOneOrFail(id, {
+       relations:['images'] 
+      });
+
+    return response.json(orphanageView.render(orphanage));
+  },
+  async create(request:Request,response:Response){
+    
+  const { 
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekend,
+  } = request.body;
+
+  const orphanagesRespository = getRepository(Orphanage);
+
+  const requestImages = request.files as Express.Multer.File[];
+
+
+  const images = requestImages.map(image => {
+    return { path: image.filename }
+  })
+
+  const data = {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekend,
+    images
+  };
+
+  const schema = Yup.object().shape({
+    name: Yup.string().required(),
+    latitude: Yup.number().required(),
+    longitude: Yup.number().required(),
+    about: Yup.string().required().max(300),
+    instructions: Yup.string().required(),
+    opening_hours: Yup.string().required(),
+    open_on_weekend: Yup.boolean().required(),
+    image: Yup.array(
+      Yup.object().shape({
+        path: Yup.string().required()
+      })
+    )
+  });
+
+  await schema.validate( data,{
+    abortEarly: false,
+  });
+
+  const orphanage = orphanagesRespository.create(data);
+
+  await orphanagesRespository.save(orphanage);
+
+  return response.status(201).json(orphanage);
+  }
+}
